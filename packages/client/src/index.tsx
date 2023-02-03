11df4518bd2653dd31a61b64986fbecc3c49c44b
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-02-03 13:20:44 +0300

    225-60 [UI] Настроить конфиги. Настроены конфиги джеста, тс, добавлены правила линтера и притиера, добавлен еслинтигнор.

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Normalize } from 'styled-normalize';
-import { GlobalStyles } from './GlobalStyles';
 import App from './components/App/App';
+import { GlobalStyles } from './GlobalStyles';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement as HTMLElement);
