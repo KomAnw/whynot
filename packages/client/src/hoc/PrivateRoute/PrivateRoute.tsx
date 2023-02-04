69196032be55f0394ecde866e3362497ece670c4
gkomissarov <komissarov2525@gmail.com>
2023-02-05 01:32:12 +0300

    routing and some consts

---
+++ packages/client/src/hoc/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { paths } from 'src/components/App/constants';
+
+const { login } = paths;
+
+const PrivateRoute = () => {
+  // TODO useAuth hook *const auth = useAuth();*
+  const auth = false;
+  return auth ? <Outlet /> : <Navigate to={login} />;
+};
+
+export default PrivateRoute;
