19fec22c523722c6784f78ebe1e31fca73e23bc5
gkomissarov <komissarov2525@gmail.com>
2023-02-05 01:32:58 +0300

    eslint ignore no-restricted-exports

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import store from 'src/store';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { Normalize } from 'styled-normalize';
 import App from './components/App/App';
 import App from './components/App/App';
 import { GlobalStyles } from './GlobalStyles';
-import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 
 const rootElement = document.getElementById('root');
