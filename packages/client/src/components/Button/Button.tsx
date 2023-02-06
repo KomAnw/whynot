a6ce95829bc394a3f221412670d687390cd66ba5
Mikhail <mixakun@gmail.com>
2023-02-06 16:33:44 +0400

    Change theme logic

--- packages/client/src/components/Button/Button.tsx
+++ packages/client/src/components/Button/Button.tsx
@@ -1,49 +1,44 @@
+import { ReactNode } from 'react';
 import styled from 'styled-components';
-import { InputHTMLAttributes } from 'react';
 
-interface ButtonProps extends InputHTMLAttributes<HTMLButtonElement> {
+type ButtonProps = {
   type?: 'submit' | 'reset' | 'button';
-  variant?: 'primary' | 'secondary';
-}
+  variant: 'primary' | 'secondary';
+  theme?: any;
+  children: ReactNode | string;
+  onSubmit: () => void;
+  onClick: () => void;
+};
 
-export const Button = (props: ButtonProps) => {
-  const { type, variant, children, onSubmit, onClick } = props;
+type ButtonComponent = ButtonProps & HTMLButtonElement;
 
+export const Button = ({ type, theme, variant = 'primary', children, onSubmit, onClick }: ButtonComponent) => {
   return (
-    <StyledButton type={type} variant={variant} onSubmit={onSubmit} onClick={onClick}>
+    <StyledButton type={type} theme={theme} variant={variant} onSubmit={onSubmit} onClick={onClick}>
       {children}
     </StyledButton>
   );
 };
 
 const StyledButton = styled.button<ButtonProps>`
-  color: ${props => props.theme.color};
-
-  background: ${props => props.theme.background};
-  background: ${props => (props.primary ? '#6C5BC3' : null)};
-  background: ${props => (props.secondary ? '#6BD35A' : null)};
-
-  font-size: ${props => props.primary};
-  font-size: ${props => (props.primary ? '32px' : null)};
-  font-size: ${props => (props.secondary ? '35px' : null)};
-
   height: '48px';
   width: '156px';
-
-  font-family: ${'Handjet' || 'Arial'};
-  font-style: 'normal';
-  font-weight: '700';
-  line-height: '39px';
   border-radius: 12px;
   border: medium none;
   cursor: pointer;
   transition: 0.5s;
+  font-style: 'normal';
+  font-weight: '700';
+  line-height: '39px';
+
+  color: ${({ theme, variant }) => theme[variant].button.color || 'black'};
+  background: ${({ theme, variant }) => theme[variant].button.background || 'red'};
+  font-size: ${({ theme, variant }) => theme[variant].button.fontSize || '32px'};
+  font-family: ${({ theme, variant }) => theme[variant].button.fontFamily || 'Arial'};
+
   &:hover,
   &:focus,
   &:active {
-    background: ${props => props.theme.hoverBackground};
-    background: ${props => (props.primary ? '#897CCF' : null)};
-    background: ${props => (props.secondary ? '#89DC7B' : null)};
-
+    background: ${({ theme, variant }) => theme[variant]?.button.hoverBackground || 'grey'};
   }
 `;
