490f90d08d109dffcd020b64a151ee955a74d859
Mikhail <mixakun@gmail.com>
2023-02-07 00:13:20 +0400

    Update page for error 500

--- packages/client/src/components/App/App.tsx
+++ packages/client/src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from 'src/hoc/Layout';
 import PrivateRoute from 'src/hoc/PrivateRoute';
-import { SignIn, SignUp, Welcome } from 'src/pages';
+import { SignIn, SignUp, Welcome, Page500 } from 'src/pages';
 import { paths } from './constants';
 
-const { login, registration } = paths;
+const { welcome, login, registration, page500 } = paths;
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           <Route index element={<Welcome />} />
           <Route path={login} element={<SignIn />} />
           <Route path={registration} element={<SignUp />} />
-          <Route path="*" element={<div>404</div>} />
+          <Route path={page500} element={<Page500 />} />
 
           {/* Приватные */}
           <Route path="/game" element={<PrivateRoute />}>
