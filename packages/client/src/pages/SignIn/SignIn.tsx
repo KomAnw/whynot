b1d973b72f9a5ee3c8d87b48c55aa3541c443152
m0000amir <mukhtarov.amir.a@gmail.com>
2023-02-07 20:04:48 +0300

    added Login label

--- packages/client/src/pages/SignIn/SignIn.tsx
+++ packages/client/src/pages/SignIn/SignIn.tsx
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+
 const SignIn = () => {
-  return <div>sign in</div>;
+  return (
+    <Wrapper>
+      <LoginComponent>
+        <Label>Login</Label>
+      </LoginComponent>
+    </Wrapper>
+  );
 };
 
 export default SignIn;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: ${({ theme }) => theme.fonts.main};
+`;
+
+const LoginComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 402px;
+  height: 372px;
+  box-shadow: 0 0 6px ${({ theme }) => theme.colors.core.background.primary};
+  border-radius: 20px;
+  color: #000;
+`;
+
+const Label = styled.label`
+  margin: 14px;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 45px;
+  color: ${({ theme }) => theme.colors.core.text.primary};
+`;
