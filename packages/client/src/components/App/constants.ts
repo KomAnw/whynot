701e3fd57c751211c05b2dd10b440b95c097f422
Mikhail <mixakun@gmail.com>
2023-02-07 20:44:50 +0400

    Fix after review

--- packages/client/src/components/App/constants.ts
+++ packages/client/src/components/App/constants.ts
@@ -19,8 +19,48 @@ export const themes = {
   default: {
     name: 'default',
     colors: {
-      text: 'white',
-      background: 'black',
+      core: {
+        background: {
+          primary: '#D9D9D9',
+          secondary: '#5BCDC9',
+          tertiary: '#F2CE0D',
+        },
+        text: {
+          primary: '#6C5BC3',
+          secondary: '#FFFFFF',
+          error: '#FF0000',
+          tertiary: '#6B687D',
+          quaternary: '#000000',
+        },
+        link: {
+          link: '#6457B8',
+          linkHover: '#6457b8b3',
+        },
+      },
+      control: {
+        input: {
+          background: '#ABCDCE',
+          label: '#6C5BC3',
+          placeHolder: '#6B687D',
+        },
+        toggle: {
+          control: '#FFFFFF',
+          backgroundPrimary: '#6BD35A',
+          backgroundSecondary: '#F2CE0D',
+        },
+        button: {
+          primary: {
+            color: '#FFFFFF',
+            background: '#6C5BC3',
+            hoverBackground: 'rgba(108, 91, 195, 0.8)',
+          },
+          secondary: {
+            color: '#FFFFFF',
+            background: '#6BD35A',
+            hoverBackground: 'rgba(107, 211, 90, 0.8)',
+          },
+        },
+      },
     },
     fonts: {
       first: '1',
@@ -31,8 +71,48 @@ export const themes = {
   other: {
     name: 'other',
     colors: {
-      text: 'black',
-      background: 'white',
+      core: {
+        background: {
+          primary: '#D9D9D9',
+          secondary: '#5BCDC9',
+          tertiary: '#F2CE0D',
+        },
+        text: {
+          primary: '#6C5BC3',
+          secondary: '#FFFFFF',
+          error: '#FF0000',
+          tertiary: '#6B687D',
+          quaternary: '#000000',
+        },
+        link: {
+          link: '#6457B8',
+          linkHover: '#6457b8b3',
+        },
+      },
+      control: {
+        input: {
+          background: '#ABCDCE',
+          label: '#6C5BC3',
+          placeHolder: '#6B687D',
+        },
+        toggle: {
+          control: '#FFFFFF',
+          backgroundPrimary: '#6BD35A',
+          backgroundSecondary: '#F2CE0D',
+        },
+        button: {
+          primary: {
+            color: '#FFFFFF',
+            background: '#6C5BC3',
+            hoverBackground: 'rgba(108, 91, 195, 0.8)',
+          },
+          secondary: {
+            color: '#FFFFFF',
+            background: '#6BD35A',
+            hoverBackground: 'rgba(107, 211, 90, 0.8)',
+          },
+        },
+      },
     },
     fonts: {
       first: '1',
--- packages/client/src/hoc/ThemeWrapper/types.ts
+++ packages/client/src/hoc/ThemeWrapper/types.ts
@@ -7,8 +7,48 @@ export type ThemeWrapperProps = {
 export type Theme = {
   name: string;
   colors: {
-    text: string;
-    background: string;
+    core: {
+      background: {
+        primary: string;
+        secondary: string;
+        tertiary: string;
+      };
+      text: {
+        primary: string;
+        secondary: string;
+        error: string;
+        tertiary: string;
+        quaternary: string;
+      };
+      link: {
+        link: string;
+        linkHover: string;
+      };
+    };
+    control: {
+      input: {
+        background: string;
+        label: string;
+        placeHolder: string;
+      };
+      toggle: {
+        control: string;
+        backgroundPrimary: string;
+        backgroundSecondary: string;
+      };
+      button: {
+        primary: {
+          color: string;
+          background: string;
+          hoverBackground: string;
+        };
+        secondary: {
+          color: string;
+          background: string;
+          hoverBackground: string;
+        };
+      };
+    };
   };
   fonts: {
     first: string;
