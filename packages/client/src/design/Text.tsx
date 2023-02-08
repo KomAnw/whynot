c89efcea517e78842d18c1a9f7f6e68ff9e7c4ac
Mikhail <mixakun@gmail.com>
2023-02-08 16:13:28 +0400

    fix

--- packages/client/src/design/Text.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { Typography } from './Typography';
-
-export const Label = styled(Typography)`
-  font-style: normal;
-  font-weight: 500;
-  font-size: 35px;
-  line-height: 39px;
-`;
--- packages/client/src/pages/SignIn/SignIn.tsx
+++
@@ -1,5 +0,0 @@
-const SignIn = () => {
-  return <div>sign in</div>;
-};
-
-export default SignIn;
