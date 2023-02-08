9a8d195ce6a696e55c072a6fe6202a209a60a862
Mikhail <mixakun@gmail.com>
2023-02-08 16:27:26 +0400

    fix

--- packages/client/src/design/ListText.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { Typography } from './Typography';
-
-export const Label = styled(Typography).attrs({ as: 'li' })`
-  font-style: normal;
-  font-weight: 700;
-  font-size: 30px;
-  line-height: 34px;
-`;
--- packages/client/src/design/Typography.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const Typography = styled('p')`
-  font-family: ${({ theme }) => theme.fonts.main};
-`;
