600dffe0a3ce2c5c8212672c4a0d272e1fcbe21b
Mikhail <mixakun@gmail.com>
2023-02-08 12:25:57 +0400

    fix types of  'type'

--- packages/client/src/components/Button/types.ts
+++ packages/client/src/components/Button/types.ts
@@ -1,7 +1,7 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 export type ButtonProps = {
-  type?: 'submit' | 'reset' | 'button';
+  type?: ButtonHTMLAttributes<HTMLButtonElement>['type'];
   variant: 'primary' | 'secondary';
   children?: ReactNode;
   onSubmit: () => void;
