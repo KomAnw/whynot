676275b47ec53000daa9309fdfb3cd25be313ffa
Mikhail <mixakun@gmail.com>
2023-02-08 10:45:38 +0400

    Fix after rebase

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -4,8 +4,8 @@ import store from 'src/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { Normalize } from 'styled-normalize';
-import App from './components/App/App';
 import { GlobalStyles } from './GlobalStyles';
+import App from './components/App/App';
 import ThemeWrapper from './hoc/ThemeWrapper/ThemeWrapper';
 
 const rootElement = document.getElementById('root');
