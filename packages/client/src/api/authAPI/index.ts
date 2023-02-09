10a4ccdb546e009653d24f6fee9500c6295029a1
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-02-09 18:38:32 +0300

    225-2 Аутентификация и Авторизация Реализован механизм аутентификации.

--- packages/client/src/api/authAPI/index.ts
+++ packages/client/src/api/authAPI/index.ts
@@ -2,6 +2,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { apiSettings } from 'src/api';
 import { TSignInData, TSignUpData, TUser } from 'src/api/authAPI/models';
 
+const ROOT_AUTH_URL = 'auth';
+
+export const AUTH_ENDPOINTS = {
+  user: `${ROOT_AUTH_URL}/user`,
+  singIn: `${ROOT_AUTH_URL}/singIn`,
+  singUp: `${ROOT_AUTH_URL}/singUp`,
+  logout: `${ROOT_AUTH_URL}/logout`,
+};
+
 export const authApi = createApi({
   reducerPath: 'auth',
   baseQuery: fetchBaseQuery({
@@ -10,19 +19,19 @@ export const authApi = createApi({
   tagTypes: ['User'],
   endpoints: build => ({
     getUser: build.query<TUser, void>({
-      query: () => ({ url: 'auth/user', method: 'GET' }),
+      query: () => ({ url: AUTH_ENDPOINTS.user, method: 'GET' }),
       providesTags: () => ['User'],
     }),
     singIn: build.mutation<string, TSignInData>({
-      query: signInData => ({ url: 'auth/signin', method: 'POST', body: signInData }),
+      query: signInData => ({ url: AUTH_ENDPOINTS.singIn, method: 'POST', body: signInData }),
       invalidatesTags: ['User'],
     }),
     singUp: build.mutation<TUser, TSignUpData>({
-      query: signUpData => ({ url: 'auth/signup', method: 'POST', body: signUpData }),
+      query: signUpData => ({ url: AUTH_ENDPOINTS.singUp, method: 'POST', body: signUpData }),
       invalidatesTags: ['User'],
     }),
     logout: build.mutation<void, void>({
-      query: () => ({ url: 'auth/logout', method: 'POST' }),
+      query: () => ({ url: AUTH_ENDPOINTS.logout, method: 'POST' }),
       invalidatesTags: ['User'],
     }),
   }),
