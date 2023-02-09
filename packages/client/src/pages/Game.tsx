a9cf24a47945822ab22b6ba691ba1e826e8d557f
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-02-09 20:49:18 +0300

    225-15 Страница регистрации. Исправил опечатку

--- packages/client/src/pages/Game.tsx
+++ packages/client/src/pages/Game.tsx
@@ -1,11 +1,3 @@
-import { useEffect } from 'react';
-import { useGetUserQuery } from 'src/api/authAPI';
-
 export const Game = () => {
-  const { data } = useGetUserQuery();
-
-  useEffect(() => {
-    console.log(data);
-  }, []);
   return <div>Game page</div>;
 };
