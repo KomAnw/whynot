91fa8401786ec54e6ba4d3648a1e4e51db4be224
Mikhail Samigullin <mixakun@gmail.com>
2023-02-10 14:17:07 +0400

    Update welcome page

--- packages/client/src/pages/Welcome/Welcome.tsx
+++ packages/client/src/pages/Welcome/Welcome.tsx
@@ -1,27 +1,12 @@
 import Switch from 'src/components/Switch';
 import { H1 } from 'src/design/H1';
 import styled from 'styled-components';
-import ErrorBoundary from 'src/hoc/ErrorBoundary/ErrorBoundary';
-import { useState } from 'react';
 
 const Welcome = () => {
   return (
     <Container>
       <Text>Welcome</Text>
       <Switch />
-      <p>2 компонента в одном ErrorBoundary</p>
-      <ErrorBoundary>
-        <Elem />
-        <Elem />
-      </ErrorBoundary>
-      <p>1 компонент в другом ErrorBoundary</p>
-      <ErrorBoundary>
-        <Elem />
-      </ErrorBoundary>
-      <p>еще 1 компонент в еще одном ErrorBoundary</p>
-      <ErrorBoundary>
-       <Elem />
-      </ErrorBoundary>
     </Container>
   );
 };
@@ -41,11 +26,3 @@ const Text = styled(H1)`
   width: max-content;
   color: ${({ theme }) => theme.colors.core.text.primary};
 `;
-
-function Elem() {
-  const [count, setCount] = useState(0);
-
-  if (count === 3) throw new Error('some message');
-
-  return <div style={{fontSize: '30px'}} onClick={() => setCount(count + 1)}>{count}</div>;
-}
