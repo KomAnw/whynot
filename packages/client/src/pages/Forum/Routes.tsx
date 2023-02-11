cf900100c85e2e33e227a6acb7a0d6cd60b6e80b
German <49876296+KomAnw@users.noreply.github.com>
2023-02-11 23:11:40 +0300

    change routing (#24)
    * change routing
    
    * eslint enable
    
    * nice english
    
    * renaming
    
    * naming

---
+++ packages/client/src/pages/Forum/Routes.tsx
@@ -0,0 +1,14 @@
+import { Route } from 'react-router-dom';
+import { paths } from 'src/components/App/constants';
+import { Forum } from './Forum';
+
+const { edit, id, newForum, index } = paths.forum;
+
+export const routes = [
+  <Route index element={<Forum />} key={index} />,
+  <Route path={newForum} element={<div>Страница создания новой темы</div>} key={newForum} />,
+  <Route path={id} key={id}>
+    <Route index element={<div>Страница с обсуждениями темы1</div>} />
+    <Route path={edit} element={<div>Страница изменения темы</div>} key={edit} />
+  </Route>,
+];
