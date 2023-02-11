a2d5a3ce4c22ff48de20f7775cac973ac0687c7f
gkomissarov <komissarov2525@gmail.com>
2023-02-11 23:32:12 +0300

    import order v2

--- packages/client/src/components/App/App.tsx
+++ packages/client/src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from 'src/hoc/Layout';
 import PrivateRoute from 'src/hoc/PrivateRoute';
-import { paths } from './constants';
 import { SignIn, SignUp, Welcome, Leaderboard, ErrorPage, GameMenu } from 'src/pages';
 import NotFoundPage from 'pages/404';
 import { gameRoutes } from 'src/pages/Game';
 import { profileRoutes } from 'src/pages/Profile';
 import { forumRoutes } from 'src/pages/Forum';
+import { paths } from './constants';
 
 const { login, registration, errorPage, game, forum, leaderboard, profile, menu } = paths;
 
