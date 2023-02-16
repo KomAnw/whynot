5bdadc87441b116251ddfa6db329ec188f46ac92
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-02-16 20:33:46 +0300

    225-81 Реализации механики игры Исправил замечания ревьюера

--- packages/client/src/hooks/react/index.ts
+++ packages/client/src/hooks/react/index.ts
@@ -4,3 +4,8 @@ export const useDidMount = (fn: () => void) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(fn, []);
 };
+
+export const useWillUnmount = (fn: () => void) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => fn, []);
+};
