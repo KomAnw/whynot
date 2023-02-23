c5083563d3f6dcd03f8d2af72811dd862702c3b6
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-02-23 18:57:37 +0300

    Feature/225-102 Вроде работает

--- packages/client/src/utils/workers.ts
+++ packages/client/src/utils/workers.ts
@@ -1,12 +1,12 @@
-export const startServiceWorker = () => {
+export const startServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     const onLoad = async () => {
       try {
         const registration = await navigator.serviceWorker.register('/sw.js');
 
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      } catch (e) {
-        console.warn('ServiceWorker registration failed: ', e);
+      } catch (error) {
+        console.warn('ServiceWorker registration failed: ', error);
       }
     };
 
