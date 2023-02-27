7404299f0b5cd0f9306e9156f2984d748d3e49df
Mikhail Samigullin <mixakun@gmail.com>
2023-02-27 14:22:21 +0400

    fix

--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -2,12 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import { userApi } from 'src/api/user/user';
 import { authApi } from 'src/api/auth/auth';
 import themeReducer from 'src/hoc/ThemeWrapper/themeSlice';
-import mockThemeReducer from 'src/hoc/MockThemeWrapper/mockThemeSlice';
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
-    mockTheme: mockThemeReducer,
     [authApi.reducerPath]: authApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
   },
