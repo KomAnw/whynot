bec9b27f69cb683b27e441b55d088da876a243c6
gkomissarov <komissarov2525@gmail.com>
2023-03-02 15:54:29 +0300

    check v2

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -5,17 +5,13 @@ dotenv.config();
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  roots: ['src'],
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
-  transform: {
-    '^.+\\.tsx$': 'ts-jest',
-  },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   moduleNameMapper: {
     '^src(.*)$': '<rootDir>/src$1',
     '^components(.*)$': '<rootDir>/src/components$1',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'identity-obj-proxy',
   },
 };
