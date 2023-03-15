d19f6ef1fde716f0a64c01d8016f1be335be799c
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-03-15 13:39:57 +0300

    Добавить OAuth авторизацию feature/225-40 Заменил строку на url и URLSearchParams

--- packages/client/src/api/oauth/oauth.ts
+++ packages/client/src/api/oauth/oauth.ts
@@ -9,14 +9,14 @@ export const OAUTH_ENDPOINTS = {
   service: `${ROOT_OAUTH_URL}/yandex/service-id`,
 };
 
-export const REDIRECT_URL = 'http://localhost:3000/oauth';
+export const REDIRECT_URL = new URL('http://localhost:3000/oauth').toString();
 
 export const redirectToOAuthYandex = (serviceId: string) => {
   const url = new URL('https://oauth.yandex.ru/authorize');
   const params = new URLSearchParams({
     response_type: 'code',
-    client_id: serviceId.toString(),
-    redirect_uri: REDIRECT_URL.toString(),
+    client_id: serviceId,
+    redirect_uri: REDIRECT_URL,
   });
 
   window.location.href = `${url}?${params}`;
