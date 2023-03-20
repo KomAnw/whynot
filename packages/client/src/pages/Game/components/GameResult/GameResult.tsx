a8c732df0f3585966ffd0a4e3c130e52df100bdc
Amir <mukhtarov.amir.a@gmail.com>
2023-03-20 08:53:12 +0300

    Feature/225 25 leaderboard api (#56)
    * start check reducer
    
    * fixed ws for leaderboardApi
    
    * added score from end game to leaderboardApi
    
    * added score to leaderboard page
    
    * fixed adding score in game end
    
    * fixed types
    
    * minor commit
    
    * fixed endpoints
    
    * fixed typing
    
    * fixed Add (endpoint)
    
    * Убран then
    
    ---------
    
    Co-authored-by: German <49876296+KomAnw@users.noreply.github.com>
    Co-authored-by: Yuri Kosenko <kosurij.dm@yandex.ru>

--- packages/client/src/pages/Game/components/GameResult/GameResult.tsx
+++ packages/client/src/pages/Game/components/GameResult/GameResult.tsx
@@ -1,11 +1,32 @@
 import GameOver from 'src/components/GameOver';
 import Portal from 'components/Portal';
+import { useGetUserQuery } from 'src/api/auth/auth';
+import {  useAddMutation } from 'src/api/leaderboard/leaderboard';
+import { LEADERBOARD_TEAM_NAME } from 'src/api/leaderboard/leaderboard';
 import { GameResultProps } from './types';
 
 const GameResult = ({ score, isWon, setIsPopupOpen, startGameAgain }: GameResultProps) => {
   const onClick = () => {
+    addLeaderHandler();
     setIsPopupOpen(false);
     startGameAgain();
+  };
+  const [addLeader] = useAddMutation();
+  const { data: user } = useGetUserQuery();
+  const addLeaderHandler = async (): Promise<void> => {
+    try {
+      await addLeader({
+        data: {
+          user_id: user!.id,
+          first_name: user!.first_name,
+          score,
+        },
+        ratingFieldName: 'score',
+        teamName: LEADERBOARD_TEAM_NAME,
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -6,6 +6,7 @@ import themeReducer from 'src/hoc/ThemeWrapper/themeSlice';
 import modeReducer from 'pages/Game/modeSlice';
 import fullscreenReducer from 'pages/Settings/fullscreenSlice';
 import gamepadReducer from 'pages/Settings/gamepadSlice';
+import { leaderboardApi } from 'src/api/leaderboard/leaderboard';
 
 const store = configureStore({
   reducer: {
@@ -16,9 +17,15 @@ const store = configureStore({
     [authApi.reducerPath]: authApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
     [oauthApi.reducerPath]: oauthApi.reducer,
+    [leaderboardApi.reducerPath]: leaderboardApi.reducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(authApi.middleware, userApi.middleware, oauthApi.middleware),
+    getDefaultMiddleware().concat(
+      leaderboardApi.middleware,
+      authApi.middleware,
+      userApi.middleware,
+      oauthApi.middleware
+    ),
 });
 
 export default store;
