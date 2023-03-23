356f9c27eb855b6f03c24973a528dcdbf986027a
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-03-23 22:30:12 +0300

    Test 4

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/__tests__/**/*.test.{ts,tsx}'],
+  globals: {
+    __SERVER_PORT__: process.env.SERVER_PORT,
+  },
 };
