056e75a5b370f5300c8eb5adde04cbec02996665
gkomissarov <komissarov2525@gmail.com>
2023-03-24 08:52:33 +0300

    fix styles

--- packages/client/src/ssr.tsx
+++ packages/client/src/ssr.tsx
@@ -3,15 +3,18 @@ import { StaticRouter } from 'react-router-dom/server';
 import { Provider } from 'react-redux';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 import App from 'components/App/App';
+import { Normalize } from 'styled-normalize';
 import ErrorBoundary from './hoc/ErrorBoundary';
 import ThemeWrapper from './hoc/ThemeWrapper/ThemeWrapper';
 import store from './store';
+import { GlobalStyles } from './components/App/GlobalStyles';
 
 export function render(url: string) {
   const sheet = new ServerStyleSheet();
-
-  const html = renderToString(
-    <StyleSheetManager sheet={sheet.instance}>
+  const AppNode = () => (
+    <>
+      <Normalize />
+      <GlobalStyles />
       <Provider store={store}>
         <StaticRouter location={url}>
           <ThemeWrapper>
@@ -21,6 +24,11 @@ export function render(url: string) {
           </ThemeWrapper>
         </StaticRouter>
       </Provider>
+    </>
+  );
+  const html = renderToString(
+    <StyleSheetManager sheet={sheet.instance}>
+      <AppNode />
     </StyleSheetManager>
   );
   const css = sheet.getStyleTags();
