e8b1931d6bb132ab06801bc9bd92245ddab19a03
Kirill Zhidkov <kirill-info@yandex.ru>
2023-03-30 02:43:14 +0300

    solutions that work

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,50 +1,11 @@
 import cors from 'cors';
 import express from 'express';
 import { connectPostgresDB } from '../database/postgres';
-import { Post } from '../models/post';
-import { Message } from '../models/message';
 
 const app = express();
 
 app.use(cors());
-connectPostgresDB().then(async () => {
-  await Post.create({
-    text: 'string',
-    author_id: 5,
-    date: new Date(),
-  });
-
-  await Message.create({
-    text: 'string',
-    author_id: 6,
-    post_id: 1,
-    date: new Date(),
-    main_message_id: 4,
-    emojis: null,
-  });
-
-  await Message.create({
-    text: 'string',
-    author_id: 7,
-    post_id: 1,
-    date: new Date(),
-    main_message_id: null,
-    emojis: [
-      [1, 5],
-      [2, 10],
-    ],
-  });
-
-  const answer = await Message.findAll({
-    where: {
-      id: 1,
-    },
-    attributes: ['id', 'emojis'],
-  });
-
-  // eslint-disable-next-line no-console
-  console.log(77, answer);
-});
+connectPostgresDB();
 
 export const isDevelopmentMode = process.argv.includes('--NODE_ENV=development');
 export const isProductionMode = process.argv.includes('--NODE_ENV=production');
