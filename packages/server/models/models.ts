6fb4634d43dcbb923c9dbeac6cee8ec9b51728dc
Mikhail Samigullin <mixakun@gmail.com>
2023-03-29 14:45:19 +0400

    changes in model

--- packages/server/models/models.ts
+++ packages/server/models/models.ts
@@ -1,13 +1,17 @@
-// import { DataType, Model } from 'sequelize-typescript';
-// import { sequelize } from '../database/postgres';
+/*
+ * import { DataType, Model } from 'sequelize-typescript';
+ * import { sequelize } from '../database/postgres';
+ */
 
 // export class Theme extends Model {}
 
-// Theme.init(
-//   {
-//     user_id: { type: DataType.INTEGER, allowNull: false, unique: true },
-//     theme: { type: DataType.ENUM('default', 'other'), defaultValue: 'default' },
-//     mode: { type: DataType.ENUM('Doodle', 'Mario', 'Homer'), defaultValue: 'Doodle' },
-//   },
-//   { sequelize, timestamps: false, modelName: 'theme' }
-// );
+/*
+ * Theme.init(
+ *   {
+ *     user_id: { type: DataType.INTEGER, allowNull: false, unique: true },
+ *     theme: { type: DataType.ENUM('default', 'other'), defaultValue: 'default' },
+ *     mode: { type: DataType.ENUM('Doodle', 'Mario', 'Homer'), defaultValue: 'Doodle' },
+ *   },
+ *   { sequelize, timestamps: false, modelName: 'theme' }
+ * );
+ */
