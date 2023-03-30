822fa2b4ac6d61b2d0fe488546437a0e5339814d
Mikhail Samigullin <mixakun@gmail.com>
2023-03-30 19:30:22 +0400

    change to classes

--- packages/server/models/models.ts
+++ packages/server/models/models.ts
@@ -1,18 +1,23 @@
-import type { Model } from 'sequelize';
-import { DataTypes } from 'sequelize';
-import type { ModelAttributes } from 'sequelize/types';
+import { Model, DataTypes } from 'sequelize';
+import type { InferAttributes, InferCreationAttributes } from 'sequelize';
 import { sequelize } from '../database/postgres';
 
-type TTheme = {
-  user_id: number;
-  theme: string;
-  mode: string;
-};
+export class Theme extends Model<InferAttributes<Theme>, InferCreationAttributes<Theme, { omit: 'id' }>> {
+  declare id?: number;
+  declare user_id: number;
+  declare theme: string;
+  declare mode: string;
+}
 
-export const ThemeModel: ModelAttributes<Model, TTheme> = {
-  user_id: { type: DataTypes.INTEGER, unique: true },
-  theme: { type: DataTypes.ENUM('default', 'other'), defaultValue: 'default' },
-  mode: { type: DataTypes.ENUM('Doodle', 'Homer, "Mario'), defaultValue: 'Doodle' },
-};
-
-export const Theme = sequelize.define('theme', ThemeModel, { timestamps: false });
+export const ThemeModel = Theme.init(
+  {
+    user_id: { type: DataTypes.INTEGER, unique: true },
+    theme: { type: DataTypes.ENUM('default', 'other'), defaultValue: 'default' },
+    mode: { type: DataTypes.ENUM('Doodle', 'Homer, "Mario'), defaultValue: 'Doodle' },
+  },
+  {
+    sequelize,
+    tableName: 'theme',
+    timestamps: false,
+  }
+);
--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -5,7 +5,10 @@ import { connectPostgresDB } from '../database/postgres';
 const app = express();
 
 app.use(cors());
-connectPostgresDB();
+
+(async () => {
+  await connectPostgresDB();
+})();
 
 export const isDevelopmentMode = process.argv.includes('--NODE_ENV=development');
 export const isProductionMode = process.argv.includes('--NODE_ENV=production');
