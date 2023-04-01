82d0582b79a80e333e8f1f3a2bf700bc76878b27
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-04-01 23:09:34 +0300

    Добавить CSP (Content-Security-Policy) Добавил различные мидлвары, связанные с безопасностью.

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,28 +1,24 @@
-import cors from 'cors';
 import express from 'express';
-import cookieParser from 'cookie-parser';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import { connectPostgresDB } from '../database/postgres';
+import * as dotenv from 'dotenv';
+import { cors, cookieParser, proxy, helmet, rateLimit } from '../middlewares';
+import { findFile } from '../utils/findFile';
+
+const middlewares = [cors, cookieParser, helmet, rateLimit];
+
+dotenv.config({ path: findFile('.env') });
+
+const { SERVER_PORT } = process.env;
 
 const app = express();
 
-app.use(cors());
-app.use(cookieParser());
-app.use(
-  '/api/v2',
-  createProxyMiddleware({
-    changeOrigin: true,
-    target: 'https://ya-praktikum.tech',
-    cookieDomainRewrite: {
-      '*': '',
-    },
-  })
-);
+app.use(middlewares);
 
-connectPostgresDB();
+app.use('/api/v2/*', proxy);
+
+// connectPostgresDB();
 
 export const isDevelopmentMode = process.argv.includes('--NODE_ENV=development');
 export const isProductionMode = process.argv.includes('--NODE_ENV=production');
-export const PORT = Number(process.env.SERVER_PORT) || 3001;
+export const PORT = Number(SERVER_PORT) || 3001;
 
 export default app;
