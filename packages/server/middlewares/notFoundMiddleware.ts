34931f0fd9a983d77664bd46b3a4909734df7d1c
Kirill Zhidkov <kirill-info@yandex.ru>
2023-04-03 18:03:50 +0300

    fix bug 6

--- packages/server/middlewares/notFoundMiddleware.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from 'express';
-
-const notFoundMiddleware = (_req: Request, res: Response) => {
-  res.status(404).json({ message: 'Not Found' });
-};
-
-export default notFoundMiddleware;
