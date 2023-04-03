aec91c1f0b4dd86f0fa38663370d9406af130adf
efes2002 <65197828+efes2002@users.noreply.github.com>
2023-04-03 20:09:16 +0300

    feature/225-160-Controller-Router-Message (#80)
    * 1
    
    * fix bug 4
    
    * fix bug
    
    * fix bug 7
    
    ---------
    
    Co-authored-by: Kirill <efes2002@gmail.com>

--- packages/server/controllers/type.ts
+++ packages/server/controllers/type.ts
@@ -1,5 +1,22 @@
 import type { Request } from 'express';
 
+type IMessage = {
+  text: string;
+  authorId: number;
+  postId: number;
+  date: Date;
+  mainMessageId: number | null;
+  emojis: number[][] | null;
+};
+
+export interface IRequestPostMessage extends Request {
+  body: IMessage;
+}
+
+export interface IRequestGetAllMessageByIdPost extends Request {
+  query: { postId: string };
+}
+
 type IPost = {
   text: string;
   authorId: number;
--- packages/server/routes/index.ts
+++ packages/server/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import { routerMessage } from './messages';
 import { routerPost } from './posts';
 import { notFoundMiddleware } from '../middlewares';
 
 export const routerApi = Router();
+
+routerApi.use('/messages', routerMessage);
 
 routerApi.use('/posts', routerPost);
 
