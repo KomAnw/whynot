1bd95b408a25f30672d07006fed8eb3cfad809a5
Mikhail Samigullin <mixakun@gmail.com>
2023-04-04 10:49:35 +0400

    add route for theme on server

--- packages/server/routes/index.ts
+++ packages/server/routes/index.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import { routerMessage } from './messages';
 import { routerPost } from './posts';
 import { notFoundMiddleware } from '../middlewares';
+import { routerTheme } from './theme';
 
 export const routerApi = Router();
 
 routerApi.use('/messages', routerMessage);
 
 routerApi.use('/posts', routerPost);
+
+routerApi.use('/theme', routerTheme);
 
 routerApi.use('/', notFoundMiddleware);
