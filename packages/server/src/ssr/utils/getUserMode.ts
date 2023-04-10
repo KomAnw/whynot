fc0167406fb4f1caec513dfb1f0205485db389e2
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-04-10 12:13:03 +0300

    Добавлены переносы строк.

--- packages/server/src/ssr/utils/getUserMode.ts
+++ packages/server/src/ssr/utils/getUserMode.ts
@@ -4,6 +4,7 @@ export const getUserMode = async (id: number | string) => {
   if (!id) {
     return null;
   }
+
   const data = await ModeModel.findOne({ where: { userId: Number(id) } });
 
   return data ? data.mode : null;
--- packages/server/src/ssr/utils/getUserTheme.ts
+++ packages/server/src/ssr/utils/getUserTheme.ts
@@ -4,6 +4,7 @@ export const getUserTheme = async (id: number | string) => {
   if (!id) {
     return null;
   }
+
   const data = await ThemeModel.findOne({ where: { userId: Number(id) } });
 
   return data ? data.theme : null;
