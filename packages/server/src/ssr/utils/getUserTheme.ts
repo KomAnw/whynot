fe916b5a69484ebf1c0c0bd48feaaeae0370758d
Mikhail Samigullin <109307026+MikhailSamigullin@users.noreply.github.com>
2023-04-10 13:21:33 +0400

    add check on null and undefined (#95)
    * add check on null and undefined
    
    * Добавлены переносы строк.
    
    ---------
    
    Co-authored-by: Yuri Kosenko <kosurij.dm@yandex.ru>

--- packages/server/src/ssr/utils/getUserTheme.ts
+++ packages/server/src/ssr/utils/getUserTheme.ts
@@ -1,6 +1,10 @@
 import { ThemeModel } from '../../../models/theme';
 
 export const getUserTheme = async (id: number | string) => {
+  if (!id) {
+    return null;
+  }
+
   const data = await ThemeModel.findOne({ where: { userId: Number(id) } });
 
   return data ? data.theme : null;
