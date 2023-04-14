f28ecd1b2c0e83527919013b686535083c8faabe
Yuri Kosenko <kosurij.dm@yandex.ru>
2023-04-15 00:55:29 +0300

    Чутка доработал

--- packages/server/src/ssr/configs/production.ts
+++ packages/server/src/ssr/configs/production.ts
@@ -7,7 +7,9 @@ const rootDir = isDockerBuild ? '../../../' : '../../../../';
 const nodeModulesPath = join(__dirname, rootDir, 'node_modules');
 const clientPath = join(nodeModulesPath, 'client');
 const distPath = isDockerBuild ? join(__dirname, rootDir, 'client-dist') : join(clientPath, 'dist');
-const ssrClientPath = isDockerBuild ? join(__dirname, rootDir, 'dist-ssr') : join(clientPath, 'dist-ssr', 'client.cjs');
+const ssrClientPath = isDockerBuild
+  ? join(__dirname, rootDir, 'dist-ssr', 'client.cjs')
+  : join(clientPath, 'dist-ssr', 'client.cjs');
 
 isProductionMode && app.use(express.static(distPath));
 
