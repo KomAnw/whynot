da599662585d46838885cee6e813f84ec23e8580
Kirill Zhidkov <kirill-info@yandex.ru>
2023-04-19 14:29:46 +0300

    add

--- packages/server/middlewares/index.ts
+++ packages/server/middlewares/index.ts
@@ -41,7 +41,7 @@ const helmet = helmetMiddleware({
   contentSecurityPolicy: {
     directives: {
       scriptSrc: ["'self'", `'nonce-${nonce}'`],
-      connectSrc: ["'self'", 'ws://localhost:*', 'http://localhost:*'],
+      connectSrc: ["'self'", 'ws://localhost:*', 'http://localhost:*', 'http://why-not.fun:*'],
     },
   },
 });
